b0770eb07b8c3e386f0663a85cf0ed1a160b34cd
vamsichinnu <vamsichinnu@missionrnd.com>
2016-03-20 17:40:48 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -20,6 +20,114 @@ struct transaction {
 	char description[20];
 };
 
+int positioncheck(struct transaction *Arr, char *date, int mid)
+{
+	int i, temp = 0;
+	for (i = 6; date[i] != '\0'; i++)
+	{
+		if (date[i] == Arr[mid].date[i])
+		{
+			temp = 1;
+		}
+		else if (date[i] > Arr[mid].date[i])
+		{
+			temp = 0;
+			break;
+		}
+		else
+		{
+			temp = -1;
+			break;
+		}
+	}
+	if (temp == 1)
+	{
+		if (date[3] * 10 + date[4] == (Arr[mid].date[3] * 10) + Arr[mid].date[4])
+		{
+			temp = 1;
+		}
+		else if (date[3] * 10 + date[4] > (Arr[mid].date[3] * 10) + Arr[mid].date[4])
+		{
+			temp = 0;
+		}
+		else
+		{
+			temp = -1;
+		}
+	}
+	if (temp == 1)
+	{
+		if (date[0] * 10 + date[1] == (Arr[mid].date[0] * 10) + Arr[mid].date[1])
+		{
+			temp = 1;
+		}
+		else if (date[0] * 10 + date[1] > (Arr[mid].date[0] * 10) + Arr[mid].date[1])
+		{
+			temp = 0;
+		}
+		else
+		{
+			temp = -1;
+		}
+	}
+	return temp;
+}
+int binarysearch(struct transaction *Arr, int min, int max, char *date, int len)
+{
+	int mid, temp, i;
+	mid = (min + max) / 2;
+	while (min<max)
+	{
+		temp = positioncheck(Arr, date, mid);
+		if (temp == 1)
+		{
+			if (positioncheck(Arr, date, mid + 1) == 1)
+			{
+				min = mid + 1;
+				break;
+			}
+			else
+			{
+				return len - mid - 1;
+			}
+		}
+
+		else if (temp == 0)
+		{
+			min = mid + 1;
+		}
+		else
+		{
+			max = mid - 1;
+		}
+		mid = (min + max) / 2;
+	}
+
+	if (min == max&&min != 0 && max != len - 1)
+	{
+		return len - mid - 1;
+	}
+	else if (min == 0)
+	{
+		return len;
+	}
+	else if (max == len - 1)
+	{
+		return 0;
+	}
+
+}
+
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+
+	int p;
+	if (Arr&&len>0)
+	{
+		p = binarysearch(Arr, 0, len - 1, date, len);
+		return p;
+	}
+	else
+	{
+		return -1;
+	}
 }
